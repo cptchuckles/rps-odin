a0532b3f1901bfecabbbdc315ecff5558dbca800
cptchuckles <image1996@gmail.com>
2019-01-23 11:13:01 -0600

    Update nomencalture

--- rps.js
+++ rps.js
@@ -3,18 +3,24 @@ let user = document.getElementById("User");
 let score = document.getElementById("Scoreboard");
 
 const list_name = ["Rock","Paper","Scissors","Lizard","Spock"];
-const list = [0b00110, 0b10001, 0b01010, 0b01001, 0b10100];
+const list_wins = [0b00110, 0b10001, 0b01010, 0b01001, 0b10100];
 
-let Roll = function() {
-    let npc = Math.floor(Math.random() * list.length);
+let RPSLS = function() {
+    let npc = Math.floor(Math.random() * 5);
+    let usr = Number(user.value);
 
-    if(list[Number(user.value)] & (1 << (4-npc))) {
+    if(usr === npc) {
+        con.textContent = "Draw! Both sides chose " + list_name[usr] + "!";
+        return;
+    }
+
+    if(list_wins[usr] & (1 << (4-npc))) {
         con.textContent = "You Win! "
-                        + list_name[Number(user.value)]
+                        + list_name[usr]
                         + " beats " + list_name[npc] + "!";
     } else {
         con.textContent = "You Lose! "
-                        + list_name[Number(user.value)]
+                        + list_name[usr]
                         + " is beaten by " + list_name[npc] + "!";
 
     }
